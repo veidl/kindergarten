56d21246b6f8d70c96ec4c31160d95e5201cf8ca
veidl <thomas.polzer@protonmail.com>
2024-01-13 15:28:08 +0100

    add push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from './shared/backend.service';
+import { OneSignal } from 'onesignal-ngx';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,14 @@ import { BackendService } from './shared/backend.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private backendService: BackendService) {}
+  constructor(
+    private backendService: BackendService,
+    private oneSignal: OneSignal
+  ) {
+    this.oneSignal.init({
+      appId: '49d63dce-923d-44dd-aaf8-81ccb8d4e982',
+    });
+  }
 
   ngOnInit(): void {
     this.backendService.getKindergardens();
